c10b140f9a560f92c7f9b0cb971ba4bea9caa054
Jason Chang <jasonchang@apjcorp.com>
2023-04-08 22:26:51 +0800

    add prism

--- Presentation/Books.Wpf/App.xaml.cs
+++ Presentation/Books.Wpf/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -11,7 +13,16 @@ namespace Books.Wpf
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
     }
 }
