8cf2c021d3fefd508048a76047d98221975a6089
zhengmaoch <zhengmaoch@msn.com>
2023-05-07 19:33:22 +0800

    add ef for sqlite

--- Libraries/Books.Core/Domain/Book.cs
+++ Libraries/Books.Core/Domain/Book.cs
@@ -8,6 +8,13 @@ namespace Books.Core.Domain
 {
     public partial class Book : BaseEntity
     {
+        public string Title { get;set; }
+
+        public string Author { get;set; } 
+        public string Publisher { get;set; }
+
+        public DateTime PublisherTime { get; set; }
+        public string Description { get;set; }
 
     }
 }
--- Libraries/Books.Data/BooksContext.cs
+++ Libraries/Books.Data/BooksContext.cs
@@ -1,9 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Books.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 
 namespace Books.Data
 {
     public class BooksContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(ConfigurationManager.ConnectionStrings["BooksDatabase"].ConnectionString);
+        }
 
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Book>(entity => { 
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Title).IsRequired();
+                entity.Property(e => e.Author).IsRequired();
+                entity.Property(e => e.Publisher).IsRequired();
+            });
+        }
     }
 }
